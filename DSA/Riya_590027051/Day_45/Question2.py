d6a034639212bac7b161997e839d2070415f2f06
RiyaKedia <kediariya5@gmail.com>
2026-07-29 22:07:19 +0530

    Day 45 Submission - Riya - 590027051

---
+++ DSA/Riya_590027051/Day_45/Question2.py
@@ -0,0 +1,55 @@
+def merge_and_count(arr, temp, left, mid, right):
+    i = left
+    j = mid + 1
+    k = left
+    inv_count = 0
+
+    while i <= mid and j <= right:
+        if arr[i] <= arr[j]:
+            temp[k] = arr[i]
+            i += 1
+        else:
+            temp[k] = arr[j]
+            inv_count += (mid - i + 1)
+            j += 1
+        k += 1
+
+    while i <= mid:
+        temp[k] = arr[i]
+        i += 1
+        k += 1
+
+    while j <= right:
+        temp[k] = arr[j]
+        j += 1
+        k += 1
+
+    for idx in range(left, right + 1):
+        arr[idx] = temp[idx]
+
+    return inv_count
+
+
+def merge_sort_and_count(arr, temp, left, right):
+    inv_count = 0
+
+    if left < right:
+        mid = (left + right) // 2
+
+        inv_count += merge_sort_and_count(arr, temp, left, mid)
+        inv_count += merge_sort_and_count(arr, temp, mid + 1, right)
+        inv_count += merge_and_count(arr, temp, left, mid, right)
+
+    return inv_count
+
+
+def count_inversions(arr):
+    temp = [0] * len(arr)
+    return merge_sort_and_count(arr, temp, 0, len(arr) - 1)
+
+
+# Input
+arr = list(map(int, input("Enter the array: ").split()))
+
+# Output
+print(count_inversions(arr))
